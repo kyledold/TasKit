4ee0f245ad2ddfb3de79747b1cf93fbf6575f92e
Kyle <kdold02@gmail.com>
2021-01-31 16:16:43 +0000

    Added workspace and empty project

---
+++ TaskIt/TaskIt/Source/TaskItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskItApp.swift
+//  TaskIt
+//
+//  Created by Kyle Dold on 31/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TaskItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
