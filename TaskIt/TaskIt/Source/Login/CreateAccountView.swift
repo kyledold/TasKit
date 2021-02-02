e4343c7e7846d8f5ffc1ad8d646b5bfbf5bbf25c
Kyle <kdold02@gmail.com>
2021-02-02 18:04:58 +0000

    Added SignIn and CreateAccount views with basic navigation

---
+++ TaskIt/TaskIt/Source/Login/CreateAccountView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountView.swift
+//  TaskIt
+//
+//  Created by Kyle Dold on 02/02/2021.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountView()
+    }
+}
---
+++ TaskIt/TaskIt/Source/Login/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  TaskIt
+//
+//  Created by Kyle Dold on 02/02/2021.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
