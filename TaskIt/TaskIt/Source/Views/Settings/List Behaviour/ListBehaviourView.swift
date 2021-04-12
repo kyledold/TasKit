c73bab4c2b27cb828ed8e41f7220a332e834d729
Kyle <kdold02@gmail.com>
2021-04-12 16:48:37 +0100

    Added rough UI for "List Behaviour" settings page

--- TaskIt/TaskIt/Source/Views/Settings/List Behaviour/ListBehaviourView.swift
+++ TaskIt/TaskIt/Source/Views/Settings/List Behaviour/ListBehaviourView.swift
@@ -8,8 +8,92 @@
 import SwiftUI
 
 struct ListBehaviourView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: Layout.Spacing.cozy) {
+            List {
+                subTaskSection
+                reminderSection
+                unfinishedTasksSection
+                completeSubTasksSection
+            }
+            .listStyle(InsetGroupedListStyle())
+        }
+        .navigationBarTitle("List Behaviour")
+    }
+    
+    private var subTaskSection: some View {
+        Section(footer: subTaskSectionFooter) {
+            HStack {
+                Text("Show Sub-tasks")
+                Spacer()
+                Toggle(isOn: .constant(false), label: {})
+                    .toggleStyle(SwitchToggleStyle(tint: .t_action))
+                    .padding(Layout.Spacing.tight)
+            }
+            .frame(height: 40)
+        }
+    }
+    
+    private var subTaskSectionFooter: some View {
+        Text("If enabled this will show the sub tasks under the related task in the list up to a maximum of 8.")
+            .padding(.horizontal)
+    }
+    
+    private var reminderSection: some View {
+        Section(footer: reminderSectionFooter) {
+            HStack {
+                Text("Show reminder")
+                Spacer()
+                Toggle(isOn: .constant(false), label: {})
+                    .toggleStyle(SwitchToggleStyle(tint: .t_action))
+                    .padding(Layout.Spacing.tight)
+            }
+            .frame(height: 40)
+        }
+    }
+    
+    private var reminderSectionFooter: some View {
+        Text("If enabled this will show the reminder time next to the task name in the list.")
+            .padding(.horizontal)
+    }
+    
+    private var unfinishedTasksSection: some View {
+        Section(footer: unfinishedTasksSectionFooter) {
+            HStack {
+                Text("Carry forward incomplete tasks")
+                Spacer()
+                Toggle(isOn: .constant(false), label: {})
+                    .toggleStyle(SwitchToggleStyle(tint: .t_action))
+                    .padding(Layout.Spacing.tight)
+            }
+            .frame(height: 40)
+        }
+    }
+    
+    private var unfinishedTasksSectionFooter: some View {
+        Text("If enabled this will carry forward any uncompleted tasks to the current date.")
+            .padding(.horizontal)
+    }
+    
+    private var completeSubTasksSection: some View {
+        Section(footer: completeSubTasksSectionFooter) {
+            HStack {
+                Text("Auto complete task")
+                Spacer()
+                Toggle(isOn: .constant(false), label: {})
+                    .toggleStyle(SwitchToggleStyle(tint: .t_action))
+                    .padding(Layout.Spacing.tight)
+            }
+            .frame(height: 40)
+        }
+    }
+    
+    private var completeSubTasksSectionFooter: some View {
+        Text("If enabled this automatically complete task when all sub-tasks are completed.")
+            .padding(.horizontal)
     }
 }
 
--- TaskIt/TaskIt/Source/Views/Settings/Sync/SyncSettingsView.swift
+++ TaskIt/TaskIt/Source/Views/Settings/Sync/SyncSettingsView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct SyncSettingsView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: Layout.Spacing.cozy) {
+            List {
+                
+            }
+            .listStyle(InsetGroupedListStyle())
+        }
+        .navigationBarTitle("TasKit Sync")
     }
 }
 
