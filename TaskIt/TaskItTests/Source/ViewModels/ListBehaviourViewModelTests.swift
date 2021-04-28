999859cacf6ba342ea5ffb9c7c00739743e9ef27
Kyle <kdold02@gmail.com>
2021-04-28 16:25:26 +0100

    Added some basic unit tests for settings related viewmodels

---
+++ TaskIt/TaskItTests/Source/ViewModels/ListBehaviourViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  ListBehaviourViewModelTests.swift
+//  TaskItTests
+//
+//  Created by Kyle Dold on 28/04/2021.
+//
+
+import XCTest
+@testable import TaskIt
+
+class ListBehaviourViewModelTests: XCTestCase {
+    
+}
+
